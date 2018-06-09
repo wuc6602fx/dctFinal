75bf8ef6515824bb062e70a8048fca7ba5f00c1c
wuc6602fx <cat5530xm@gmail.com>
2018-06-09 23:15:25 +0800

    套用警察毒販圖

--- public/Lab7.js
+++ public/Lab7.js
@@ -11,16 +11,16 @@ $(document).ready(() => { // jQuery main
         createjs.Ticker.framerate = 60;
         // load assets
         repo.loadManifest([
-            {id: 'criminal', src: "images/m1.png"},//criminals
+            {id: 'criminal', src: "images/drugs_man.png"},//criminals
             {id: 'm2', src: "images/m2.png"},//man
             {id: 'm3', src: "images/m3.png"},
             {id: 'm4', src: "images/m4.png"},
-            {id: 'plane', src: "images/plane.jpg"},//police
+            {id: 'plane', src: "images/police.png"},//police
             {id: 'explode', src: '/images/exp.png'},//bullet explode
             {id: 'startB', src: '/images/startB.png'},
             {id: 'main', src: '/images/main.jpg'},
             {id: 'bg', src: '/images/bg.jpg'}
-            ]);
+        ]);
         repo.on('complete', addTitleView);
     }
 
@@ -49,10 +49,11 @@ $(document).ready(() => { // jQuery main
         //     console.log("press");
         //     createjs.Tween.get(TitleView).to({y:-320}, 300).call(draw);
         // }
-        startB.on("click", function(evt) {
-            createjs.Tween.get(TitleView).to({y:-320}, 300).call(draw);
+        startB.on("click", function (evt) {
+            createjs.Tween.get(TitleView).to({y: -320}, 300).call(draw);
         });
     }
+
     //
     function draw() {
 
@@ -60,10 +61,10 @@ $(document).ready(() => { // jQuery main
         stage.removeChild(TitleView);
         TitleView = null;
         //ticker
-            // createjs.Ticker.addEventListener("tick", handleTick);
-            // function handleTick(event) {
-            //
-            // }
+        // createjs.Ticker.addEventListener("tick", handleTick);
+        // function handleTick(event) {
+        //
+        // }
 
         //
 
@@ -77,22 +78,28 @@ $(document).ready(() => { // jQuery main
         // random() run only once?///
         /////////////////////////////////////
         //console.log(theHeight)
+        //resize image
+        plane.set({scaleX: 0.1, scaleY: 0.1})
+        criminals[0].set({scaleX: 0.1, scaleY: 0.1})
+        exp.set({scaleX: 0.5, scaleY: 0.5})
+
         plane.set({x: 10, y: 10});
         stage.addChild(plane);
 
-        function animate(){
+        function animate() {
             var theHeight = Math.floor((((Math.random() * 5) + 1) * 50));
-            criminals[0].set({x: 720, y: theHeight});//x will be replaced to canvas.width
+            criminals[0].set({x: canvas.width, y: theHeight});
             stage.addChild(criminals[0]);
 
             createjs.Tween.get(criminals[0])
-                .to({x: 280, y: theHeight}, 1000)
+                .to({x: 280, y: theHeight}, 2500)
                 .call(() => {
                     stage.removeChild(criminals[0]);
                     animate();
                 });
 
         }
+
         animate();
 
 
@@ -107,7 +114,7 @@ $(document).ready(() => { // jQuery main
                     break;
                 case 32:
                     let dot = new createjs.Shape();
-                    dot.graphics.beginFill('black').drawCircle(plane.x + plane.image.width, plane.y + plane.image.height / 2, 5);
+                    dot.graphics.beginFill('black').drawCircle(plane.x + (plane.image.width) * plane.scaleX + 10, plane.y + (plane.image.height) * plane.scaleY / 6, 2);
                     createjs.Tween.get(dot)
                         .to({x: canvas.width}, 1000)
                         .call(() => {
