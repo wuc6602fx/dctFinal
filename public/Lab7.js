a9dc89263911aee28c9f60475ba874c43c4a4605
wuc6602fx <cat5530xm@gmail.com>
2018-06-10 20:36:28 +0800

    碰撞偵測(毒販取消Tween動畫)

--- public/Lab7.js
+++ public/Lab7.js
@@ -3,11 +3,23 @@ $(document).ready(() => { // jQuery main
     let stage = new createjs.Stage(canvas);
     let repo = new createjs.LoadQueue();
     let startB;
-
+    let criminals = [];
+    let speedX = -1;//criminal speed
+    let endX = 280;//end line to disappear
+    let gameStart = false;
+    let police;
+    let exp;
+    let shooting = false;
+    let kills = 0;
 
     function setup() {
         // automatically update
-        createjs.Ticker.on("tick", e => stage.update());
+        /*
+        createjs.Ticker.on("tick", (e) => {
+            stage.update();
+        });
+        */
+        createjs.Ticker.on("tick", tick);
         createjs.Ticker.framerate = 60;
         // load assets
         repo.loadManifest([
@@ -15,7 +27,7 @@ $(document).ready(() => { // jQuery main
             {id: 'm2', src: "images/m2.png"},//man
             {id: 'm3', src: "images/m3.png"},
             {id: 'm4', src: "images/m4.png"},
-            {id: 'plane', src: "images/police.png"},//police
+            {id: 'police', src: "images/police.png"},//police
             {id: 'explode', src: '/images/exp.png'},//bullet explode
             {id: 'startB', src: '/images/startB.png'},
             {id: 'main', src: '/images/main.jpg'},
@@ -49,12 +61,48 @@ $(document).ready(() => { // jQuery main
         //     console.log("press");
         //     createjs.Tween.get(TitleView).to({y:-320}, 300).call(draw);
         // }
-        startB.on("click", function (evt) {
+        startB.on("click", function (event) {
+
             createjs.Tween.get(TitleView).to({y: -320}, 300).call(draw);
+
         });
     }
 
-    //
+    function createCriminals() {
+
+        for (let i = 0; i < 5; i++) {
+            let criminal = new createjs.Bitmap(repo.getResult('criminal'));
+            criminal.set({scaleX: 0.1, scaleY: 0.1});
+            criminals.push(criminal);
+        }
+        let theHeight = Math.floor(Math.random() * 200);
+        criminals[0].set({x: stage.canvas.width, y: theHeight});
+        stage.addChild(criminals[0]);
+
+    }
+
+    function tick() {
+        if (gameStart) {
+            criminals[0].x += speedX;
+            if (criminals[0].x < endX) {
+                criminals[0].x = stage.canvas.width;
+            }
+            if (shooting) {
+                console.log("shooting!");
+                console.log(criminals[0].y + " " + criminals[0].image)
+                if (police.y > criminals[0].y - 20 && police.y < criminals[0].y + 85) {// why criminals[0].image.y = 0?
+                    console.log("hit!");
+                    kills += 1;
+                    criminals[0].x = stage.canvas.width;
+
+                }
+            }
+        }
+
+        stage.update();
+    }
+
+
     function draw() {
 
         //console.log("call draw");
@@ -66,65 +114,43 @@ $(document).ready(() => { // jQuery main
         //
         // }
 
-        //
+        police = new createjs.Bitmap(repo.getResult('police'));
+        exp = new createjs.Bitmap(repo.getResult('explode'));
 
-        let criminals = [new createjs.Bitmap(repo.getResult('criminal')),
-            new createjs.Bitmap(repo.getResult('m2')),
-            new createjs.Bitmap(repo.getResult('m3')),
-            new createjs.Bitmap(repo.getResult('m4'))];
-        let plane = new createjs.Bitmap(repo.getResult('plane'));
-        let exp = new createjs.Bitmap(repo.getResult('explode'));
-        //////////////////////////////////////
-        // random() run only once?///
-        /////////////////////////////////////
-        //console.log(theHeight)
         //resize image
-        plane.set({scaleX: 0.1, scaleY: 0.1})
-        criminals[0].set({scaleX: 0.1, scaleY: 0.1})
-        exp.set({scaleX: 0.5, scaleY: 0.5})
-
-        plane.set({x: 10, y: 10});
-        stage.addChild(plane);
-
-        function animate() {
-            var theHeight = Math.floor((((Math.random() * 5) + 1) * 50));
-            criminals[0].set({x: canvas.width, y: theHeight});
-            stage.addChild(criminals[0]);
-
-            createjs.Tween.get(criminals[0])
-                .to({x: 280, y: theHeight}, 2500)
-                .call(() => {
-                    stage.removeChild(criminals[0]);
-                    animate();
-                });
-
-        }
-
-        animate();
+        police.set({scaleX: 0.1, scaleY: 0.1});
+        exp.set({scaleX: 0.5, scaleY: 0.5});
 
 
+        police.set({x: 10, y: 10});
+        stage.addChild(police);
+        createCriminals();
+
+        gameStart = true;
         //控制方向
         window.addEventListener('keydown', function (e) {
             switch (e.keyCode) {
                 case 38:// up
-                    plane.y -= 10;
+                    police.y -= 10;
                     break;
                 case 40:// down
-                    plane.y += 10;
+                    police.y += 10;
                     break;
                 case 32:
                     let dot = new createjs.Shape();
-                    dot.graphics.beginFill('black').drawCircle(plane.x + (plane.image.width) * plane.scaleX + 10, plane.y + (plane.image.height) * plane.scaleY / 6, 2);
+                    dot.graphics.beginFill('black').drawCircle(police.x + (police.image.width) * police.scaleX + 10, police.y + (police.image.height) * police.scaleY / 6, 2);
                     createjs.Tween.get(dot)
-                        .to({x: canvas.width}, 1000)
+                        .to({x: stage.canvas.width}, 100)
                         .call(() => {
                             stage.removeChild(dot);
                             exp.x = 500;
-                            exp.y = plane.y;
+                            exp.y = police.y;
                             stage.addChild(exp);
+                            shooting = false;
 
                         }).wait(500).call(() => stage.removeChild(exp));
                     stage.addChild(dot);
+                    shooting = true;
                     break;
             }
         });
