ae8f263af313118251a4778ebe23a2440aa32b7a
wuc6602fx <cat5530xm@gmail.com>
2018-06-13 19:43:14 +0800

    0613_first

--- public/Lab7.js
+++ public/Lab7.js
@@ -17,12 +17,17 @@ $(document).ready(() => { // jQuery main
     let shooting = false;
     let kills = 0;//人數
     let isCounted = false;//防止重複計數
-    const topBarHeight = 100;
+    const topBarHeight = 250;
     let isAppear = [1, 1, 1];//0:毒販重生中，打到不會計分，1:計分
     let nextTimeAppear = [0, 0, 0];
     let criminalsAppearFrequency = 50; //毒販生成頻率
     let criminalsAvgSpeed = 2;//毒販走路速度
     let time = 0;  //total elapsed game time
+    let backgroundPlaying;
+    let backgroundBuilding;
+    let passby;
+    let isPassbyAppear = false;
+    let criminal;
 
     //計數器
     let counter = new createjs.Text(new String(kills), "20px Arial", "black"),
@@ -51,7 +56,10 @@ $(document).ready(() => { // jQuery main
             {id: 'explode', src: '/images/exp.png'},//bullet explode
             {id: 'startB', src: '/images/startB.png'},
             {id: 'main', src: '/images/main.jpg'},
-            {id: 'bg', src: '/images/bg.jpg'}
+            {id: 'bg', src: '/images/bg.jpg'},
+            {id: 'backgroundPlaying', src: '/images/background_playing.png'},
+            {id: 'backgroundBuilding', src: '/images/background_house.png'},
+            {id: 'passby', src: '/images/stranger_girl.png'}
         ]);
         repo.on('complete', addTitleView);
     }
@@ -88,7 +96,7 @@ $(document).ready(() => { // jQuery main
     function createCriminals() {
 
         for (let i = 0; i < 3; i++) {
-            let criminal = new createjs.Bitmap(repo.getResult('criminal'));
+            criminal = new createjs.Bitmap(repo.getResult('criminal'));
             criminal.set({scaleX: 0.1, scaleY: 0.1});
             criminals.push(criminal);
             //let theHeight = (Math.floor(Math.random() * 100) % 3) * criminal.image.height * criminal.scaleY * 1.4 + topBarHeight;//1.4為毒販間距
@@ -104,29 +112,27 @@ $(document).ready(() => { // jQuery main
     function tick() {//update every second   //call 60 times per second
 
         //time's up, game over, move to finalScreen
-        time +=1;
-        console.log(time);
-        if(time === 800){   //600加上多遊戲介紹時間，最好要從gameStart後開始算時間
-            createjs.Ticker.off("tick", tick);
-            createjs.Tween.get(nothing).to({y: -320}, 300).call(moveToFinalScreen());
 
-        }
+
         //
 
 
         if (gameStart) {
+
             for (let i = 0; i < 3; i++) {
                 if (nextTimeAppear[i] === 0) {//重生時間到
                     isAppear[i] = 1;
                     speedX[i] = -1 * (Math.floor(Math.random() * 10) % 3 + criminalsAvgSpeed);
                 }
             }
+            //createjs.Tween.get(backgroundBuilding, {loop: true}).to({x: 0}, 3000);
             counter.text = "Kill: " + kills;
             criminals[0].x += speedX[0];
             criminals[1].x += speedX[1];
             criminals[2].x += speedX[2];
             for (let i = 0; i < 3; i++) {
                 if (criminals[i].x < endX) {
+                    kills -= 1;
                     criminals[i].x = stage.canvas.width;
                 }
             }
@@ -147,7 +153,14 @@ $(document).ready(() => { // jQuery main
                             speedX[i] = 0;
                             nextTimeAppear[i] = ((Math.floor(Math.random() * 10) % 3 + 1) * criminalsAppearFrequency);
                         }
+
                         criminals[i].x = stage.canvas.width;
+                        if (isPassbyAppear) {
+                            criminals[0] = criminal;
+                            criminals[1] = criminal;
+                            criminals[2] = criminal;
+                            isPassbyAppear = false;
+                        }
                     }
                 }
             }
@@ -176,16 +189,21 @@ $(document).ready(() => { // jQuery main
         //
         // }
 
-        stage.addChild(counter);
+
+        backgroundPlaying = new createjs.Bitmap(repo.getResult('backgroundPlaying'));
+        backgroundBuilding = new createjs.Bitmap(repo.getResult('backgroundBuilding'));
         police = new createjs.Bitmap(repo.getResult('police'));
         exp = new createjs.Bitmap(repo.getResult('explode'));
 
         //resize image
         police.set({scaleX: 0.1, scaleY: 0.1});
         exp.set({scaleX: 0.5, scaleY: 0.5});
-
-
-        police.set({x: 10, y: 10});
+        backgroundBuilding.set({x: stage.canvas.width, y: 30, scaleX: 0.5, scaleY: 0.5});
+        backgroundPlaying.set({x: 0, y: 0});
+        stage.addChild(backgroundPlaying);
+        stage.addChild(backgroundBuilding);
+        stage.addChild(counter);
+        police.set({x: 10, y: topBarHeight});
         stage.addChild(police);
         //createCriminals();
         teachTheGame();
@@ -222,15 +240,19 @@ $(document).ready(() => { // jQuery main
         //createjs.Ticker.on("tick", tick);  //for test
         //createjs.Ticker.framerate = 60;
         createCriminals();
-
+        createjs.Tween.get(backgroundBuilding, {loop: true}).to({x: -1 * backgroundBuilding.image.width / 2}, 6000);
         //控制方向
         window.addEventListener('keydown', function (e) {
             switch (e.keyCode) {
                 case 38:// up
-                    police.y -= 10;
+                    if (police.y > topBarHeight - 50) {
+                        police.y -= 10;
+                    }
                     break;
                 case 40:// down
-                    police.y += 10;
+                    if (police.y < 600) {
+                        police.y += 10;
+                    }
                     break;
                 case 32:
                     bullet = new createjs.Shape();
@@ -251,7 +273,8 @@ $(document).ready(() => { // jQuery main
             }
         });
     }
-    function moveToFinalScreen(){
+
+    function moveToFinalScreen() {
         stage.removeAllChildren();
         stage.update();
 
